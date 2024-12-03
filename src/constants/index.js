287e79d46892a3d024649a3b7e881be2dcce833e
Yashkumar1312 <yashkumar131201@gmail.com>
2024-12-03 15:09:44 +0530

    Adding Manvi and Keya name as a Preference

--- src/constants/index.js
+++ src/constants/index.js
@@ -23,7 +23,7 @@ import {
     jobit,
     tripguide,
     threejs,
-    django,python
+    django,python,
   } from "../assets";
   
   export const navLinks = [
@@ -185,22 +185,22 @@ import {
       name: "Vaibhav Purohit",
       designation: "Team Lead",
       company: "Tecblic Private Ltd",
-      image: "https://randomuser.me/api/portraits/women/4.jpg",
-    },
-    {
-      testimonial:
-        "I've never met a web developer who truly cares about their clients' success like Rick does.",
-      name: "Chris Brown",
-      designation: "COO",
-      company: "DEF Corp",
       image: "https://randomuser.me/api/portraits/men/5.jpg",
     },
     {
       testimonial:
-        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-      name: "Lisa Wang",
-      designation: "CTO",
-      company: "456 Enterprises",
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Manvi Chaudhari",
+      designation: "Finance Analyst",
+      company: "Patel Jewellers",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "After Yash optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Keya Patel",
+      designation: "Chief financial officer",
+      company: "Kartiki Enterprices",
       image: "https://randomuser.me/api/portraits/women/6.jpg",
     },
   ];
